f90c7c2a3d567370e4b8dd85f94a212c3160bd8b
Scott Wedge <scott.wedge@gmail.com>
2022-03-24 23:54:36 -0400

    Replace 'capitalize' with slice to convert first letter of words but leave remaining letters case as is

--- convert_string_to_camel_case/script.py
+++ convert_string_to_camel_case/script.py
@@ -6,15 +6,15 @@ def split_and_cap(text, sepr):
     # Split on character ("-") into list of strings
     text_list = text.split(sep = sepr)
 
-    # Capitalize all except first word in list
+    # Capitalize first letter of all except first word in list
+    # cannot use 'capitalize' method since it converts upper case inside word to lower case
     for j in range(1,len(text_list)):
-        text_list[j] = text_list[j].capitalize()
+        text_list[j] = text_list[j][0].upper() + text_list[j][1:]
          
     # Add all words together into a new string
     new_text = text_list[0]  # Init string 
     for j in range(1, len(text_list)):
         new_text = new_text + text_list[j]
-    
     return new_text
     
 
