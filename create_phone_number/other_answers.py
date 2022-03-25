914c0a085f5cdff80e9ff139f6c09e3661219e14
Scott Wedge <scott.wedge@gmail.com>
2022-03-25 14:47:54 -0400

    Other solutions to create phone number from list of values

---
+++ create_phone_number/other_answers.py
@@ -0,0 +1,27 @@
+# Answers from others:
+
+def create_phone_number(n):
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+
+def create_phone_number(n):
+    n = ''.join(map(str,n))
+    return '(%s) %s-%s'%(n[:3], n[3:6], n[6:])
+
+def create_phone_number(n):
+  str1 =  ''.join(str(x) for x in n[0:3])
+  str2 =  ''.join(str(x) for x in n[3:6])
+  str3 =  ''.join(str(x) for x in n[6:10])
+  return '({}) {}-{}'.format(str1, str2, str3)
+
+def create_phone_number(n):
+    m = ''.join(map(str, n))
+    return f"({m[:3]}) {m[3:6]}-{m[6:]}"
+
+def create_phone_number(n):
+    n = "".join([str(x) for x in n] )
+    return("(" + str(n[0:3]) + ")" + " " + str(n[3:6]) + "-" + str(n[6:]))
+
+def create_phone_number(n):
+    return "(%i%i%i) %i%i%i-%i%i%i%i" % tuple(n)
+
+create_phone_number = lambda n: f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
