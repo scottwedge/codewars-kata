2fec65382b3691a1af304c0a0f31f6ee7e254f64
Scott Wedge <scott.wedge@gmail.com>
2022-03-29 21:10:27 -0400

    Others nicer solutions

---
+++ pig_latin/others_solutions.py
@@ -0,0 +1,20 @@
+
+def pig_it(text):
+    lst = text.split()
+    return ' '.join( [word[1:] + word[:1] + 'ay' if word.isalpha() else word for word in lst])
+
+
+def pig_it(text):
+    return " ".join(x[1:] + x[0] + "ay" if x.isalnum() else x for x in text.split())
+
+
+def pig_it(text):
+    res = []
+    
+    for i in text.split():
+        if i.isalpha():
+            res.append(i[1:]+i[0]+'ay')
+        else:
+            res.append(i)
+            
+    return ' '.join(res)
