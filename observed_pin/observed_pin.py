ffe06ebb5d74ed85955477d66d08d20df05ef5ab
Scott Wedge <scott.wedge@gmail.com>
2022-04-02 23:00:25 -0400

    Code to generate PINs seems to work but tests fail since order in list differs. Committing code in current state before edit it

---
+++ observed_pin/observed_pin.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+# The observed PIN
+# 
+# Alright, detective, one of our colleagues successfully observed our target person, Robby the robber. We followed him to a secret warehouse, where we assume to find all the stolen stuff. The door to this warehouse is secured by an electronic combination lock. Unfortunately our spy isn't sure about the PIN he saw, when Robby entered it.
+# 
+# The keypad has the following layout:
+# 
+# ┌───┬───┬───┐
+# │ 1 │ 2 │ 3 │
+# ├───┼───┼───┤
+# │ 4 │ 5 │ 6 │
+# ├───┼───┼───┤
+# │ 7 │ 8 │ 9 │
+# └───┼───┼───┘
+#     │ 0 │
+#     └───┘
+# 
+# He noted the PIN 1357, but he also said, it is possible that each of the digits he saw could actually be another adjacent digit (horizontally or vertically, but not diagonally). E.g. instead of the 1 it could also be the 2 or 4. And instead of the 5 it could also be the 2, 4, 6 or 8.
+# 
+# He also mentioned, he knows this kind of locks. You can enter an unlimited amount of wrong PINs, they never finally lock the system or sound the alarm. That's why we can try out all possible (*) variations.
+# 
+# * possible in sense of: the observed PIN itself and all variations considering the adjacent digits
+# 
+# Can you help us to find all those variations? It would be nice to have a function, that returns an array (or a list in Java/Kotlin and C#) of all variations for an observed PIN with a length of 1 to 8 digits. We could name the function getPINs (get_pins in python, GetPINs in C#). But please note that all PINs, the observed one and also the results, must be strings, because of potentially leading '0's. We already prepared some test cases for you.
+# 
+# Detective, we are counting on you!
+
+# expectations = [('8', ['5','7','8','9','0']),
+#                ('11',["11", "22", "44", "12", "21", "14", "41", "24", "42"]),
+#                ('369', ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"])]
+
+def test_8():
+    assert get_pins("8") ==  ('8', ['5','7','8','9','0'])
+
+def test_11():
+    assert get_pins("11") ==  ('11',["11", "22", "44", "12", "21", "14", "41", "24", "42"])
+
+def test_369():
+    assert get_pins("369") ==   ('369', ["339","366","399","658","636","258","268","669","668","266","369","398","256","296","259","368","638","396","238","356","659","639","666","359","336","299","338","696","269","358","656","698","699","298","236","239"])
+
+
+def generate_pins_list(observed, possibles):
+    print("DEBUG__ observed= ", observed)  #DEBUG
+    print("DEBUG__ possibles= ", possibles)  #DEBUG
+    list_of_pins = []
+    if len(observed) == 1: list_of_pins = possibles[0]
+    elif len(observed) == 2:
+        for j in possibles[0]:
+#            print("DEBUG ____ for j in: ", possibles[0])  #DEBUG
+            for k in possibles[1]:
+#                print("DEBUG ____ for k in: ", possibles[1])  #DEBUG
+                val = str(j) + str(k)
+#                print("DEBUG_____val= ", val)  #DEBUG
+                list_of_pins.append(val)
+    else:
+        pass
+    print("DEBUG___ return list_of_pins= ", list_of_pins)
+    return list_of_pins
+
+
+def get_possibles(n):
+    print("DEBUG___get_possibles_input=", n)  #DEBUG
+    if n == "1": return ["1","2","4"]
+    elif n == "2": return ["1","2","3","5"]
+    elif n == "3": return ["2","3","6"]
+    elif n == "4": return ["1","4","5","7"]
+    elif n == "5": return ["2","4","5","6","8"]
+    elif n == "6": return ["3","5","6"]
+    elif n == "7": return ["4","7","8"]
+    elif n == "8": return ["5","7","8","9","0"]
+    elif n == "9": return ["6","8","9"]
+    elif n == "0": return ["0","8"] 
+    else: 
+        return ["blah"]  # Should never get here
+
+
+
+def to_chars(pin):  # Convert input string to list of characters (ie "0456" to ["0","4","5","6"])
+    chars = []
+    for j in range(len(pin)): 
+        letter = pin[j:j+1]        
+#        print("DEBUG___letter=", letter)  #DEBUG
+        chars.append(letter)
+    print("DEBUG___returning chars=", chars)  #DEBUG
+    return chars
+
+
+def get_pins(observed):
+    chars = to_chars(observed)
+    print("DEBUG___chars=", chars)  #DEBUG
+    possibles = []
+    for j in chars:
+        possibles.append(get_possibles(j))
+    print("DEBUG___possibles=", possibles)  #DEBUG
+    list_of_pins = generate_pins_list(observed, possibles)
+    return (observed, list_of_pins)
+    
+
+
+#def main():
+#    observed = "8"
+#    (inp,pins) = get_pins(observed)
+##    print("DEBUG___pins=", pins)  #DEBUG
+##    return [observed, pins]
+
+
+#if __name__ == "__main__":
+#    main()
